9d9aa940e56dba70dc8087e433c3f0d3507d7b2f
Asich <mustafinaskar@gmail.com>
2013-08-08 18:04:06 +0600

    initial commit

---
+++ SingleViewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SingleViewTest
+//
+//  Created by Mustafin Askar on 30.04.13.
+//  Copyright (c) 2013 askar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+
+@end
